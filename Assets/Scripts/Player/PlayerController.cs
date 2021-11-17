f047600c29feb2a222195dbb7b501edc382e8761
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2021-11-17 16:30:15 +0530

    Collision Fixed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float rayCastDistance;
     [SerializeField] private float speed;
+    [SerializeField] private BoxCollider[] boxColliders;
+    [SerializeField] private float rayDistance;
+    [SerializeField] private KeyCode[] key;
+    
     
     private bool moving;
     private float angle = 90f;
     private Rigidbody rb;
+    private int lastDirection = -1;
 
     private void Start()
     {
@@ -28,32 +32,34 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+        print("test");
         moving = false;
         rb.velocity = Vector3.zero;
-    }
-
-    private void OnCollisionStay(Collision other)
-    {
-        moving = false;
-        rb.velocity = Vector3.zero;
+        other.transform.GetComponent<BoxCollider>().enabled = false;
     }
 
     private void ReadInput()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(key[0]) && !Physics.Raycast(transform.position, Vector3.right, rayDistance))
             RotateToDirection((int) Directions.Right);
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(key[1]) && !Physics.Raycast(transform.position, Vector3.left, rayDistance))
             RotateToDirection((int) Directions.Left);
-        else if (Input.GetKeyDown(KeyCode.W))
+        else if (Input.GetKeyDown(key[2]) && !Physics.Raycast(transform.position, Vector3.up, rayDistance))
             RotateToDirection((int) Directions.Up);
-        else if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(key[3]) && !Physics.Raycast(transform.position, Vector3.down, rayDistance))
             RotateToDirection((int) Directions.Down);
     }
 
-    private void RotateToDirection(float direction)
+    private void RotateToDirection(int direction)
     {
-        moving = true;
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle * direction));
+        if (lastDirection == -1 || lastDirection != direction)
+        {
+            lastDirection = direction;
+            moving = true;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle * direction));
+            foreach (var boxCollider in boxColliders)
+                boxCollider.transform.GetComponent<BoxCollider>().enabled = true;
+        }
     }
     
     private enum Directions
