7a474e5fb3dcae8cf1114c46b3a368de3ea3a93a
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2021-11-20 19:51:33 +0530

    Final

---
+++ Assets/Scripts/Levels Navigation/LevelNavigation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelNavigation : MonoBehaviour
+{
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
