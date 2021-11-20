debbb7a5916ac9f14f4c721dc3fa53eda187e847
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2021-11-20 22:12:08 +0530

    Easter Egg

---
+++ Assets/Scripts/Easter Eggs/EasterEggs.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class EasterEggs : MonoBehaviour
+{
+    public void Dishwasher()
+    {
+        Application.OpenURL("https://www.amazon.in/s?k=dishwasher&ref=nb_sb_noss_2");
+    }
+}
